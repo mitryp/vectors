e002b5e2a24e2c2c5d66a8372a08d69649147385
Dmitry Popov <77672371+MitryP@users.noreply.github.com>
2021-01-22 20:11:05 +0200

    Major update of functionality

--- vectors2d.py
+++ vectors2d.py
@@ -4,15 +4,19 @@
 """Small module that adds class Vector2D and operations with vectors.
 
 All functions take Vector2D. Also they take tuples or lists of int/float with len == 2.
-Vectors can be added, multiplied by other vector or by int/float and you can get angle between two vectors.
+Vectors can be added, subbed, multiplied by other vector or by int/float the angle between two vectors can be found.
+===
 
+abs(Vector2D) -> absolute_vector() -> float
 Vector2D + Vector2D -> sum_vectors() -> Vector2D
+Vector2D - Vector2D -> sub_vectors() -> Vector2D
 Vector2D * Vector2D -> scalar_mult_vectors() -> float
 Vector2D * int/float -> mult_vector() -> Vector2D
 get_angle(Vector2D, Vector2D) -> angle in radians
-
+===
+Extended version of documentation can be found on GitHub: https://github.com/MitryP/vectors/
 """
-from math import sqrt, acos
+from math import sqrt, acos, pi
 
 
 class Vector2D:
@@ -23,18 +27,18 @@ class Vector2D:
         :param args: list or tuple with len == 2 of int/float default = [0,0]
 
         """
-        # print(args, type(args), len(args))
+
         if len(args) == 0:
             args = [0, 0]
-            # self.x, self.y = args[0], args[1]
             self.vector = list(args)
 
         elif len(args) == 1:
-            # self.x = args[0]
             if type(args[0]) in [list, tuple]:
                 self.vector = args[0]
+
             elif type(args[0]) is type(self):
                 self.vector = args[0]()
+
             else:
                 self.vector = [args[0], 0]
 
@@ -43,12 +47,10 @@ class Vector2D:
 
         else:
             if type(args[0]) in [int, float] and type(args[1]) in [int, float]:
-                # self.x, self.y = args[0], args[1]
                 self.vector = list(args)
+
             else:
                 raise TypeError
-
-    # TODO: Do +=, -=, *= etc
 
     def __getitem__(self, key: int):
         if key > len(self.vector) - 1 or key < -1:
@@ -85,19 +87,21 @@ class Vector2D:
         else:
             return self == other
 
-    def __add__(self, other):
+    def __add__(self, other: (list, tuple, object)):
         if type(other) in (type(self), tuple, list):
             return sum_vectors(self, other)
+
         else:
             raise TypeError
 
-    def __sub__(self, other):
+    def __sub__(self, other: (list, tuple, object)):
         if type(other) in (type(self), tuple, list):
             return sub_vectors(self, other)
+
         else:
             raise TypeError
 
-    def __mul__(self, other):
+    def __mul__(self, other: (int, float, list, tuple, object)):
         """
         Returns scalar multiplication of vectors or multiplies the vector by given number
 
@@ -120,8 +124,101 @@ class Vector2D:
     def __call__(self):
         """
         :return: tuple(x, y)
+
         """
+
         return self.vector
+
+    def __neg__(self):
+        """
+        Returns vector with negative coordinates of self.
+
+        :return: Vector2D
+
+        """
+
+        result_vector = self.vector.copy()
+        result_vector[0] *= -1
+        result_vector[1] *= -1
+
+        return Vector2D(result_vector)
+
+    def iszero(self):
+        """
+        Check if the vector is zero vector.
+
+        :return: bool
+
+        """
+
+        if self.vector[0] == 0 and self.vector[1] == 0:
+            return True
+
+        else:
+            return False
+
+    def isperpendicular(self, vector: (tuple, list, object)):
+        """
+        Check if vector is perpendicular to the given vector.
+
+        :param vector: Vector2D or list or tuple
+        :return: bool
+
+        """
+
+        if type(vector) is not type(self):
+            vector = Vector2D(vector)
+
+        if self.iszero() or vector.iszero():
+            return False
+
+        if get_angle(self, vector) == pi/2:
+            return True
+
+        else:
+            return False
+
+    def isparallel(self, vector: (tuple, list, object)):
+        """
+        Check if vector is co-directional to the given vector.
+
+        :param vector: Vector2D or list or tuple
+        :return: bool
+
+        """
+
+        if type(vector) is not type(self):
+            vector = Vector2D(vector)
+
+        if self.iszero() or vector.iszero():
+            return True
+
+        elif get_angle(self, vector) in [0, pi]:
+            return True
+
+        else:
+            return False
+
+    def iscodirected(self, vector: (tuple, list, object)):
+        """
+        Check if vector is co-directed to given vector.
+
+        :param vector: Vector2D or list or tuple
+        :return: bool
+
+        """
+
+        if type(vector) is not type(self):
+            vector = Vector2D(vector)
+
+        if self.iszero() or vector.iszero():
+            return True
+
+        elif get_angle(self, vector) == 0:
+            return True
+
+        else:
+            return False
 
 
 def absolute_vector(vector: (Vector2D, list, tuple)):
@@ -130,7 +227,9 @@ def absolute_vector(vector: (Vector2D, list, tuple)):
 
     :param vector: Vector2D or list or tuple
     :return: float
+
     """
+
     if type(vector) is not Vector2D:
         vector = Vector2D(vector)
 
@@ -143,11 +242,14 @@ def sum_vectors(*vectors: (Vector2D, list, tuple)):
 
     :param vectors: Vector2D or list or tuple
     :return: Vector2D
+
     """
+
     result = [0, 0]
     for vector in vectors:
         if type(vector) is not Vector2D:
             vector = Vector2D(vector)
+
         result[0] += vector[0]
         result[1] += vector[1]
 
@@ -162,6 +264,7 @@ def sub_vectors(*vectors: (Vector2D, list, tuple)):
     :return: Vector2D
 
     """
+
     result = None
     for vector in vectors:
         if type(vector) is not Vector2D:
@@ -184,7 +287,9 @@ def mult_vector(vector: (Vector2D, tuple, list), modifier: (int, float)):
     :param vector: Vector2D or tuple or list
     :param modifier: must be number
     :return: Vector2D
+
     """
+
     if type(vector) is not Vector2D:
         vector = Vector2D(vector)
 
@@ -197,7 +302,9 @@ def scalar_mult_vectors(*vectors: (Vector2D, list, tuple)):
 
     :param vectors: Vector2D or list or tuple
     :return: float
+
     """
+
     temp = [1, 1]
     for vector in vectors:
         if type(vector) is not Vector2D:
@@ -220,14 +327,21 @@ def get_angle(vector1: (Vector2D, list, tuple), vector2: (Vector2D, list, tuple)
     :param vector1: Vector2D or list or tuple
     :param vector2: Vector2D or list or tuple
     :return: float
+
     """
+
+    # TODO: find how to calc angle with zero vectors
+
     if type(vector1) is not Vector2D:
         vector1 = Vector2D(vector1)
 
     if type(vector2) is not Vector2D:
         vector2 = Vector2D(vector2)
+    try:
+        result = scalar_mult_vectors(vector1(), vector2()) / (abs(vector1) * abs(vector2))  # cos of angle
 
-    result = scalar_mult_vectors(vector1(), vector2()) / (abs(vector1) * abs(vector2))  # cos of angle
+    except ZeroDivisionError:
+        result = 1
 
     return acos(result)
 
@@ -239,7 +353,9 @@ def vector_from_dots(dot1: (tuple, list), dot2: (tuple, list)):
     :param dot1: tuple or list
     :param dot2: tuple or list
     :return: Vector2D
+
     """
+
     if len(dot1) != 2 or len(dot2) != 2:
         raise TypeError
 
@@ -247,4 +363,5 @@ def vector_from_dots(dot1: (tuple, list), dot2: (tuple, list)):
         vector = [0, 0]
         vector[0] = dot2[0] - dot1[0]
         vector[1] = dot2[1] - dot1[1]
+
         return Vector2D(vector)
